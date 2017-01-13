77fff9c987e90639dc3a90878d51c4dbc3cb5f7b
Nikolaos Theodosis <2272891T@student.gla.ac.uk>
2017-01-13 15:56:43 +0000

    Implement categories indexing

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -9,3 +9,17 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': "Nikolaos Theodosis"}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
