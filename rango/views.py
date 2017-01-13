a182eeaf71d8505737189ba45792051f81cbf935
Nikolaos Theodosis <2272891T@student.gla.ac.uk>
2017-01-13 11:53:15 +0000

    Template for about page

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
@@ -6,4 +5,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': "Nikolaos Theodosis"}
+    return render(request, 'rango/about.html', context=context_dict)
