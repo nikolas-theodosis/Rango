f77236eff11162d17ee90b3c6d221a311a9a4fea
Nikolaos Theodosis <2272891T@student.gla.ac.uk>
2017-01-13 23:14:28 +0000

    Convert restricted page to template

--- rango/views.py
+++ rango/views.py
@@ -116,7 +116,8 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
+    # return HttpResponse("Since you're logged in, you can see this text!")
 
 @login_required
 def user_logout(request):
