9d0b2a2e979f01c8527e9348eec51112bb62d719
Nikolaos Theodosis <2272891T@student.gla.ac.uk>
2017-01-13 16:13:58 +0000

    Add most viewed pages

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rango.models import Category, Page
 
 def index(request):
+    context_dict = {}
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
