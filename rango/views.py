1bd624f52652c636cd19cd14fa19600a9ed8aafb
Nikolaos Theodosis <2272891T@student.gla.ac.uk>
2017-01-13 20:12:39 +0000

    Add categories form

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     context_dict = {}
@@ -26,3 +27,15 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
